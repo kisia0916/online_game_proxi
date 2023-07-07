47855f8123b64a07129520fe8e3f1a2caa67939b
kisia0916 <fumiharuabe@gmail.com>
2023-07-08 08:26:48 +0900

    v1.2.3

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.get("/jump",(req,res)=>{
 app.use((req,res)=>{
     res.redirect("/")
 })
-app.listen(3000 || process.env.PORT,()=>{
+app.listen(process.env.PORT||3000,()=>{
     console.log({nowdomain:domain,serverURL:serverURL})
 })

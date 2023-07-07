2f1e19f65269342c2022ce6c396c1a1a2b5c4927
kisia0916 <fumiharuabe@gmail.com>
2023-07-08 08:08:12 +0900

    v1.0

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express()
+const fs = require("fs")
+const informationFile = fs.readFileSync("./serverURL.txt","utf-8")
+const page = fs.readFileSync("./index.html","utf-8")
+
+const serverURL = informationFile.split(",")[0]
+const domain = informationFile.split(",")[1]
+console.log(serverURL,domain)
+app.get("/",(req,res)=>{
+    res.writeHead(200,{"Content-Type":"text/html"})
+    res.write(page)
+
+    res.end()
+})
+app.get("/jump",(req,res)=>{
+    res.writeHead(302,{
+        "Location":`https://${serverURL}`
+
+    })
+    res.end()
+})
+app.use((req,res)=>{
+    res.redirect("/")
+})
+app.listen(3000,()=>{
+    console.log({nowdomain:domain,serverURL:serverURL})
+})
